97ec5d12a31aa000176ddb181433d08c7405186e
Muhammad Fadilah Nurjaman <96771710+dhilz202@users.noreply.github.com>
2022-10-27 23:25:33 +0700

    Add files via upload

--- Account.java
+++ Account.java
@@ -1,64 +1,169 @@
-package Pbo;
+import java.util.Scanner;
 
-public class Account {
-    protected double balance;
-    
-    public Account(double initBalance){
-        balance = initBalance;
+public abstract class Account {
+    private int accountNumber;
+    protected double balance; 
+    public Account() {
+
     }
-    
-    public double getBalance(){
+    public Account(int accountNumber) {
+        this.accountNumber = accountNumber;
+        balance = 0; 
+    }
+    public double getBalance() {
         return this.balance;
     }
-    
-    public boolean deposit (double amt){
-        balance += amt;
-        return true;
-    }
-    
-    public boolean withdraw(double amt){
-        balance -= amt;
-        return true;
-    }
-}
 
-class SavingAccount extends Account{
-    private double interestRate;
-    
-    public SavingAccount(double balance, double interest_rate){
-        super(balance);
-        interestRate = interest_rate;
+    public int getAccountNumber() {
+        return this.accountNumber;
     }
-}
+    public abstract void deposit(double amount); 
 
-class CheckingAccount extends Account{
-    private double overdraftProtection;
-    
-    public CheckingAccount(double balance,double protect){
-        super(balance);
-        overdraftProtection= protect;
+    public abstract void withdraw(double amount); 
+    public static void main(String [] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        Account accounts [] = new Account[10];
+        int numAccounts = 0; 
+
+        int choice;
+
+        do {
+            choice = menu(keyboard);
+            System.out.println();
+            
+            if(choice == 1) {
+                accounts[numAccounts++] = createAccount(keyboard);
+            } else if(choice == 2) {
+                doDeposit(accounts, numAccounts, keyboard);
+            } else if(choice == 3) {
+                doWithdraw(accounts, numAccounts, keyboard);
+            } else if(choice == 4) {
+                applyInterest(accounts, numAccounts, keyboard);
+            } else {
+                System.out.println("GoodBye!");
+            }
+            System.out.println();
+        } while(choice != 5);
     }
-    
-    public CheckingAccount(double balance){
-        super(balance);
-        overdraftProtection=-1.0;
-    }
-    
-    @Override
-    public boolean withdraw(double amt){
-        double overdraftNeeded = amt - this.balance;
-        if (balance - amt >= 0.0) {
-            this.balance -= amt;
-            return true;
-        }else{
-            if (overdraftProtection != -1.0 || overdraftProtection < overdraftNeeded){
-                return false;
-            }else{
-                this.balance = 0.0;
-                overdraftProtection -= overdraftNeeded;
-                return true;
+
+    public static int accountMenu(Scanner keyboard) {
+        System.out.println("Select Account Type");
+        System.out.println("1. Checking Account");
+        System.out.println("2. Savings Account");
+
+        int choice;
+        do {
+            System.out.print("Enter choice: ");
+            choice = keyboard.nextInt();
+        }while(choice < 1 || choice > 2);
+        
+        return choice;
+    } 
+
+    public static int searchAccount(Account accounts [], int count, int accountNumber) {
+
+        for(int i=0; i<count; i++) {
+            if(accounts[i].getAccountNumber() == accountNumber) {
+                return i;
             }
         }
-    }
-}
 
+        return -1; 
+    }
+    public static void doDeposit(Account accounts [], int count, Scanner keyboard) {
+        
+        System.out.print("\nPlease enter account number: ");
+        int accountNumber = keyboard.nextInt();
+
+        
+        int index = searchAccount(accounts, count, accountNumber);
+
+        if(index >= 0) {
+        
+            System.out.print("Please enter Deposit Amount: ");
+            double amount = keyboard.nextDouble();
+
+            accounts[index].deposit(amount);
+        } else {
+            System.out.println("No account exist with AccountNumber: " + accountNumber);
+        }
+    }
+
+    public static void doWithdraw(Account accounts [], int count, Scanner keyboard) {
+
+        System.out.print("\nPlease enter account number: ");
+        int accountNumber = keyboard.nextInt();
+
+ 
+        int index = searchAccount(accounts, count, accountNumber);
+
+        if(index >= 0) {
+      
+            System.out.print("Please enter Withdraw Amount: ");
+            double amount = keyboard.nextDouble();
+            accounts[index].withdraw(amount);
+        } else {
+            System.out.println("No account exist with AccountNumber: " + accountNumber);
+        }
+    }
+
+    public static void applyInterest(Account accounts [], int count, Scanner keyboard) {
+        
+        System.out.print("\nPlease enter account number: ");
+        int accountNumber = keyboard.nextInt();
+
+       
+        int index = searchAccount(accounts, count, accountNumber);
+
+        if(index >= 0) {
+            
+            if(accounts[index] instanceof SavingAccount) {
+                ((SavingAccount)accounts[index]).applyInterest();
+            }
+        } else {
+            System.out.println("No account exist with AccountNumber: " + accountNumber);
+        }
+    }
+
+  
+    public static Account createAccount(Scanner keyboard) {
+
+        Account account = null; 
+        int choice = accountMenu(keyboard);
+
+        int accountNumber;
+        System.out.print("Enter Account Number: ");
+        accountNumber = keyboard.nextInt();
+
+        if(choice == 1)  { // chekcing account
+            System.out.print("Enter Transaction Fee: ");
+            double fee = keyboard.nextDouble();
+            account = new CheckingAccount(accountNumber, fee);
+        } else { // Savings account
+            
+            System.out.print("Please enter Interest Rate: ");
+            double ir = keyboard.nextDouble();
+            account = new SavingAccount(accountNumber, ir);
+        }
+        return account;
+    }
+
+    public static int menu(Scanner keyboard) {
+        System.out.println("Bank Account Menu");
+        System.out.println("1. Create New Account");
+        System.out.println("2. Deposit Funds");
+        System.out.println("3. Withdraw Funds");
+        System.out.println("4. Apply Interest");
+        System.out.println("5. Quit");
+
+        int choice;
+
+        do {
+            System.out.print("Enter choice: ");
+            choice = keyboard.nextInt();
+        } while(choice < 1 || choice > 5);
+
+        return choice;
+    } 
+}
